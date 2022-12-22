cca066d1c63f66d0f961d123ea56e49185cafd89
sshowmik <sadman.showmik@g.bracu.ac.bd>
2022-12-22 11:08:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { AppContext } from "./context/ContextAPi"
+
+function App() {
+
+  return (
+    <div className="App text-3xl">
+      <AppContext />
+    </div>
+  )
+}
+
+export default App
